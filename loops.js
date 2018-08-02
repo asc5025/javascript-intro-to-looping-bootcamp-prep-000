cd877953df87eba8c490093f06ddcf43c5380afa
Albert Chun <albert.s.chun@gmail.com>
2018-08-02 20:32:54 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -19,5 +19,5 @@ function whileLoop(number) {
 function doWhileLoop(array) {
   do {
     array.pop();
-  } while (array.length > 0 || maybeTrue());
+  } while (array.length > 0 && maybeTrue());
 } return array;
